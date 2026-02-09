4f23d7e1266047f39eeec49fe5e3e07ec5c0f2de
Nistha Diwedi <nisthadiwedi07@gmail.com>
2026-02-09 13:06:11 +0530

    Connect frontend to deployed backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://trustworthy-intuition-production-a5cd.up.railway.app/",
 });
 
 export default API;
